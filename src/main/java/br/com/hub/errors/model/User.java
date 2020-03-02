4a3db176c11ae20684b1c9634189f668a89a896a
Louise Silva <louisebahiense@outlook.com>
2020-03-02 16:00:44 -0300

    Modelo de Usuario relacionando com Log e padronização dos modelos com @Column

--- src/main/java/br/com/hub/errors/model/User.java
+++ src/main/java/br/com/hub/errors/model/User.java
@@ -1,95 +1,39 @@
 package br.com.hub.errors.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "TB_USER")
 public class User implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @NotNull
+    @Column
     private String name;
 
-    @NotNull
+    @Column(name = "lastName")
     private String lastName;
 
-    @NotNull
-    @Email
+    @Column
     private String email;
 
-    @NotNull
+    @Column
     private String password;
 
-    @NotNull
+    @Column
     private String token;
-    // o tipo correto seria lista/ mas ainda esta dando erro no banco
-    private String logList;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getLogList() {
-        return logList;
-    }
-
-    public void setLogList(String logList) {
-        this.logList = logList;
-    }
+    @OneToMany(mappedBy = "user")
+    private List<Log> logList;
 }
