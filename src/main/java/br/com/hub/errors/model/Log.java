ad3f872b8d16d8adb06cb4ba55a31a6fb2622a93
Louise Silva <louisebahiense@outlook.com>
2020-03-02 14:55:31 -0300

    Retorno do Generation Type dos ids para AUTO

--- src/main/java/br/com/hub/errors/model/Log.java
+++ src/main/java/br/com/hub/errors/model/Log.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Table(name = "TB_LOG")
 public class Log implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     private ErrorLevelsEnum errorLevel;
--- src/main/java/br/com/hub/errors/model/User.java
+++ src/main/java/br/com/hub/errors/model/User.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @NotNull
