9e92b161d10c59a5f73a512a4382f3cc837d97e7
Louise Silva <louisebahiense@outlook.com>
2020-03-12 00:38:07 -0300

    Preparação do UserRepository e UserService para encontrar Usuários

--- src/main/java/br/com/hub/errors/service/UserService.java
+++ src/main/java/br/com/hub/errors/service/UserService.java
@@ -1,4 +1,22 @@
 package br.com.hub.errors.service;
 
+import br.com.hub.errors.model.User;
+import br.com.hub.errors.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findById(UUID id){ return userRepository.findById(id);}
+    public List<User> findAll(){ return userRepository.findAll(); }
+
+
 }
