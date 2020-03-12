ef3a812faddb6a49be18c97355460ab2e037363e
Louise Silva <louisebahiense@outlook.com>
2020-03-12 01:23:07 -0300

    Criação do UserController com inclusão dos GetsMapping findAll e findById

---
+++ src/main/java/br/com/hub/errors/controller/UserController.java
@@ -0,0 +1,31 @@
+package br.com.hub.errors.controller;
+
+import br.com.hub.errors.model.User;
+import br.com.hub.errors.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value="/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> findById(UUID id){
+        return  userService.findById(id);
+    }
+
+}
