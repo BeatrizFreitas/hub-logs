cbcf6b111104375c02ac4115a9c953ed9660f9bb
beatriz.freitas <beea.15@gmail.com>
2020-03-21 18:33:10 -0300

    modificacoes no arquivo de especificacao para separar as logicas que filtra os parametros e que faz a query

--- src/main/java/br/com/hub/errors/specification/LogSpecs.java
+++ src/main/java/br/com/hub/errors/specification/LogSpecs.java
@@ -18,28 +18,33 @@ import java.util.Map;
 public class LogSpecs {
     public static Specification<Log> getLogsByFilters(Map<String, String> params){
         return new Specification<Log>() {
-            List<Predicate> predicates = new ArrayList<>();
-
             @Override
             public Predicate toPredicate(Root<Log> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                if (params.get("origin") != null && !params.get("origin").isEmpty()) {
-                    predicates.add(criteriaBuilder.equal(root.get("origin"), params.get("origin")));
-                }
-                if (params.get("date") != null && !params.get("date").isEmpty()) {
-                    predicates.add(criteriaBuilder.equal(root.get("date"), params.get("date")));
-                }
-               if (params.get("errorLevel") != null && !params.get("errorLevel").isEmpty()) {
-                    predicates.add(criteriaBuilder.equal(root.get(Log_.errorLevel), ErrorLevelsEnum.valueOf(params.get("errorLevel"))));
-                }
-                if (params.get("environment") != null && !params.get("environment").isEmpty()) {
-                    predicates.add(criteriaBuilder.equal(root.get(Log_.environment), EnvironmentEnum.valueOf(params.get("environment"))));
-                }
+                List<Predicate> predicatesList = getListOfParameters(root, criteriaBuilder, params);
 
-                Predicate predicateForFilters = criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
-
+                Predicate predicateForFilters = criteriaBuilder.and(predicatesList.toArray(new Predicate[predicatesList.size()]));
 
                 return predicateForFilters;
             }
         };
+    }
+
+    public static List<Predicate> getListOfParameters(Root<Log> root, CriteriaBuilder criteriaBuilder, Map<String, String> params) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (params.get("origin") != null && !params.get("origin").isEmpty()) {
+            predicates.add(criteriaBuilder.equal(root.get("origin"), params.get("origin")));
+        }
+        if (params.get("date") != null && !params.get("date").isEmpty()) {
+            predicates.add(criteriaBuilder.equal(root.get("date"), params.get("date")));
+        }
+        if (params.get("errorLevel") != null && !params.get("errorLevel").isEmpty()) {
+            predicates.add(criteriaBuilder.equal(root.get(Log_.errorLevel), ErrorLevelsEnum.valueOf(params.get("errorLevel"))));
+        }
+        if (params.get("environment") != null && !params.get("environment").isEmpty()) {
+            predicates.add(criteriaBuilder.equal(root.get(Log_.environment), EnvironmentEnum.valueOf(params.get("environment"))));
+        }
+
+        return predicates;
     }
 }
